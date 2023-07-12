2ca32c726d2d72219a7eccb02b2f114f1ce28e6b
Alfred Peter <alpeter91@gmail.com>
2023-07-13 00:11:46 +0300

    Add calculator child components

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,48 @@
 import PropTypes from 'prop-types';
 
-const Calculator = () => {
-  return <div>Calculator</div>;
-};
+const Calculator = () => (
+  <div>
+    <Grid />
+  </div>
+);
+
+const calculatorSymbols = [
+  'AC',
+  '+/-',
+  '%',
+  '÷',
+  '7',
+  '8',
+  '9',
+  'x',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '0',
+  '.',
+  '-',
+];
+
+const Grid = () => (
+  <div className="border" id="grid">
+    <Display />
+    {calculatorSymbols.map((symbol, index) => (
+      <Buttons symbol={symbol} key={symbol} id={index} />
+    ))}
+  </div>
+);
+
+const Display = () => (
+  <div className="border" id="display">
+    <h2>0</h2>
+  </div>
+);
+
+const Buttons = ({ symbol, id }) => <button id={'id'+ id } type="button">{symbol}</button>;
+Buttons.propTypes = { symbol: PropTypes.string.isRequired };
 export default Calculator;
