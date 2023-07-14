6a32c0cec7b97678c3e7657c3ae108c094b5b9d6
Alfred Peter <alpeter91@gmail.com>
2023-07-14 15:16:00 +0300

    Display results from API call

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 
 const Quotes = () => {
+  // states
   const [quote, setQuote] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
+
+// Api call data
   const apiKey = 'xHr8HwBU7H/MU6z8/2+9GA==VwppmyXM5Ucuxbb7';
   const category = 'computers'
   const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`
 
+  // useEffect
   useEffect(() => {
     setIsLoading(true)
     try {fetch(url, {
@@ -26,6 +30,19 @@ const Quotes = () => {
     setIsLoading(false)
   }, [])
 
-  return <div>{ quote }</div>;
+  if (hasError) return (
+    <div className='text-box' id='error'>
+      Something went wrong!
+    </div>
+  );
+
+  if(isLoading) return (
+    <div className='text-box' id='loading'>
+      Loading...
+    </div>
+  );
+
+  // Main return
+  return <div className='text-box' id='quote'>{ quote }</div>;
 };
 export default Quotes;
