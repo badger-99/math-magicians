526cc6e526df732d55fc3c5cec0d1fde40f4a158
Alfred Peter <alpeter91@gmail.com>
2023-07-14 14:00:40 +0300

    Add useEffect

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Quotes = () => {
   const [quote, setQuote] = useState('');
   const [isLoading, setIsLoading] = useState('');
   const [hasError, setHasError] = useState('');
+
+  useEffect(() => {
+    
+  }, [])
 
   return <div>{ quote }</div>;
 };
