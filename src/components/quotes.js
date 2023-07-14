aaf2e9b1c556538475a8c2ec3d371e6df7d88605
Alfred Peter <alpeter91@gmail.com>
2023-07-14 15:15:30 +0300

    Fetch data from API

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -2,11 +2,28 @@ import { useState, useEffect } from 'react';
 
 const Quotes = () => {
   const [quote, setQuote] = useState('');
-  const [isLoading, setIsLoading] = useState('');
-  const [hasError, setHasError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const apiKey = 'xHr8HwBU7H/MU6z8/2+9GA==VwppmyXM5Ucuxbb7';
+  const category = 'computers'
+  const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`
 
   useEffect(() => {
-    
+    setIsLoading(true)
+    try {fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': apiKey,
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => response.json);
+      setQuote(`${response.quote} -${response.author}`)
+    }
+    catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false)
   }, [])
 
   return <div>{ quote }</div>;
