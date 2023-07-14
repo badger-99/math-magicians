0e9d39723abaf44e2e50f1e28f9495b970a328ae
Alfred Peter <alpeter91@gmail.com>
2023-07-14 15:44:40 +0300

    Render quotes in App.js

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -6,43 +6,50 @@ const Quotes = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
 
-// Api call data
+  // Api call data
   const apiKey = 'xHr8HwBU7H/MU6z8/2+9GA==VwppmyXM5Ucuxbb7';
-  const category = 'computers'
-  const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`
+  const category = 'computers';
+  const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
 
   // useEffect
   useEffect(() => {
-    setIsLoading(true)
-    try {fetch(url, {
-      method: 'GET',
-      headers: {
-        'X-Api-Key': apiKey,
-        'Content-Type': 'application/json',
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': apiKey,
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = await result.json();
+        setQuote(`${data[0].quote} -${data[0].author}`);
+      } catch (error) {
+        setHasError(true);
       }
-    })
-      .then((response) => response.json);
-      setQuote(`${response.quote} -${response.author}`)
-    }
-    catch (error) {
-      setHasError(true);
-    }
-    setIsLoading(false)
-  }, [])
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
-  if (hasError) return (
-    <div className='text-box' id='error'>
-      Something went wrong!
-    </div>
-  );
+  if (hasError) {
+    return (
+      <div className="text-box" id="error">
+        Something went wrong!
+      </div>
+    );
+  }
 
-  if(isLoading) return (
-    <div className='text-box' id='loading'>
-      Loading...
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div className="text-box" id="loading">
+        Loading...
+      </div>
+    );
+  }
 
   // Main return
-  return <div className='text-box' id='quote'>{ quote }</div>;
+  return <div className="text-box" id="quote">{ quote }</div>;
 };
 export default Quotes;
