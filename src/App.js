7c96fe3ed54e012613b40a1b12f367f1d6888009
Alfred Peter <alpeter91@gmail.com>
2023-07-24 17:09:25 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quotes from './components/quotes';
 import './App.css';
+import Home from './components/home';
 
 const App = () => (
-  <section>
-    <Calculator />
-    <Quotes />
-  </section>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/calculator" element={<Calculator />} />
+    <Route path="/quotes" element={<Quotes />} />
+  </Routes>
 );
 
 export default App;
