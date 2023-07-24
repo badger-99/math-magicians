b5ac405dfa0a46d2746beece18ce0b0099a3d4ec
Alfred Peter <alpeter91@gmail.com>
2023-07-24 17:08:56 +0300

    Fix linter errors

--- src/components/home.js
+++ src/components/home.js
@@ -1,26 +1,24 @@
-const home = () => {
-  return (
-    <div>
-      <h2>Welcome to our page!</h2>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque,
-        iure quisquam saepe hic, animi fugit consequuntur ad reprehenderit
-        debitis beatae? Veniam culpa qui eligendi quae praesentium odit dolores
-        quibusdam nulla quos, quidem labore necessitatibus tempora autem quod
-        alias asperiores ducimus commodi, soluta cum ad ipsa vitae! Deleniti,
-        nam aut!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque,
-        iure quisquam saepe hic, animi fugit consequuntur ad reprehenderit
-        debitis beatae? Veniam culpa qui eligendi quae praesentium odit dolores
-        quibusdam nulla quos, quidem labore necessitatibus tempora autem quod
-        alias asperiores ducimus commodi, soluta cum ad ipsa vitae! Deleniti,
-        nam aut!
-      </p>
-    </div>
-  );
-}
-export default home
+const Home = () => (
+  <div>
+    <h2>Welcome to our page!</h2>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque,
+      iure quisquam saepe hic, animi fugit consequuntur ad reprehenderit
+      debitis beatae? Veniam culpa qui eligendi quae praesentium odit dolores
+      quibusdam nulla quos, quidem labore necessitatibus tempora autem quod
+      alias asperiores ducimus commodi, soluta cum ad ipsa vitae! Deleniti,
+      nam aut!
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque,
+      iure quisquam saepe hic, animi fugit consequuntur ad reprehenderit
+      debitis beatae? Veniam culpa qui eligendi quae praesentium odit dolores
+      quibusdam nulla quos, quidem labore necessitatibus tempora autem quod
+      alias asperiores ducimus commodi, soluta cum ad ipsa vitae! Deleniti,
+      nam aut!
+    </p>
+  </div>
+);
+export default Home;
