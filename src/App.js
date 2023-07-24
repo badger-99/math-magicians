3e81438d8c24fc3fd1648b6127217e38bce169fb
Alfred Peter <alpeter91@gmail.com>
2023-07-24 17:40:36 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quotes from './components/quotes';
 import './App.css';
 import Home from './components/home';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/calculator" element={<Calculator />} />
-    <Route path="/quotes" element={<Quotes />} />
-  </Routes>
+  <>
+    <nav>
+      <h3>Math Magicians</h3>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/calculator">Calculator</NavLink>
+        </li>
+        <li>
+          <NavLink to="/quote">Quote</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quotes />} />
+    </Routes>
+  </>
 );
 
 export default App;
